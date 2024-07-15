663f7128a857899320950ace2e805276c648aa92
Timo Reitz <godsboss@gmx.de>
2024-07-15 18:55:00 +0200

    Sort testcases alphabetically.

--- problem/details_test.go
+++ problem/details_test.go
@@ -12,6 +12,17 @@ import (
 
 func TestUnmarshalJSON(t *testing.T) {
 	testcases := map[string]func(t *testing.T, details problem.Details){
+		"detail": func(t *testing.T, details problem.Details) {
+			if detail := details.Detail; detail != "This API does not understand SOAP." {
+				t.Errorf("expected detail '%s', got '%s'", "This API does not understand SOAP.", detail)
+			}
+		},
+		"instance": func(t *testing.T, details problem.Details) {
+			instance := details.Instance
+			if instance != "https://api.example.org/problems/instances/666" {
+				t.Errorf("expected instance '%s', got '%s'", "https://api.example.org/problems/instances/666", instance)
+			}
+		},
 		"status_400": func(t *testing.T, details problem.Details) {
 			if status := details.Status; status != 400 {
 				t.Errorf("expected status code %d, got %d", 400, status)
@@ -25,21 +36,10 @@ func TestUnmarshalJSON(t *testing.T) {
 				t.Errorf("expected title '%s', got '%s'", "JSON broken", title)
 			}
 		},
-		"detail": func(t *testing.T, details problem.Details) {
-			if detail := details.Detail; detail != "This API does not understand SOAP." {
-				t.Errorf("expected detail '%s', got '%s'", "This API does not understand SOAP.", detail)
-			}
-		},
 		"type": func(t *testing.T, details problem.Details) {
 			typ := details.Type
 			if typ != "https://api.example.org/foo/bar/123" {
 				t.Errorf("expected type '%s', got '%s'", "https://api.example.org/foo/bar/123", typ)
-			}
-		},
-		"instance": func(t *testing.T, details problem.Details) {
-			instance := details.Instance
-			if instance != "https://api.example.org/problems/instances/666" {
-				t.Errorf("expected instance '%s', got '%s'", "https://api.example.org/problems/instances/666", instance)
 			}
 		},
 	}
