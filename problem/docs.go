b09fdfb6b3b0ddee262810cbf6f324f024a0657e
Timo Reitz <godsboss@gmx.de>
2024-07-16 20:46:44 +0200

    Add more package documentation.

--- problem/docs.go
+++ problem/docs.go
@@ -1,4 +1,10 @@
-// Package problem provides a JSON problem details implementation.
+// Package problem provides a problem details implementation.
 //
 // See the specification at https://www.rfc-editor.org/rfc/rfc9457.html
+//
+// The details type defined by this package is made to be robust and simple.
+// It provides a very rudimentary extension mechanism that serves as a base
+// for more sophisticated solutions.
+//
+// Currently, only JSON is supported. XML support may be added in the future.
 package problem
