8299007052194a9896548db136838f0611185454
Timo Reitz <godsboss@gmx.de>
2024-08-14 18:28:48 +0200

    Add SwapKeyValues.

--- seq/seq.go
+++ seq/seq.go
@@ -24,3 +24,15 @@ func WithoutKeys[Key, Value any](sequence iter.Seq2[Key, Value]) iter.Seq[Value]
 	}
 
 }
+
+// SwapKeyValues takes a sequence of key/value pairs and returns a sequence of key/value pairs
+// with keys and values being switched in their positions.
+func SwapKeyValues[Key, Value any](sequence iter.Seq2[Key, Value]) iter.Seq2[Value, Key] {
+	return func(yield func(value Value, key Key) bool) {
+		sequence(
+			func(key Key, value Value) bool {
+				return yield(value, key)
+			},
+		)
+	}
+}
--- seq/seq_example_test.go
+++ seq/seq_example_test.go
@@ -40,3 +40,20 @@ func ExampleWithoutKeys() {
 	// 22
 	// 27
 }
+
+func ExampleSwapKeyValues() {
+	ages := map[string]int{
+		"Peter": 33,
+		"Paul":  22,
+		"Mary":  27,
+	}
+
+	for age, name := range seq.SwapKeyValues(maps.All(ages)) {
+		fmt.Printf("%s is %d years old.\n", name, age)
+	}
+
+	// Unordered output:
+	// Peter is 33 years old.
+	// Paul is 22 years old.
+	// Mary is 27 years old.
+}
