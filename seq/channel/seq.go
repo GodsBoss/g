80bb75a7cc87666ddab097c26a410b8f38d417ec
Timo Reitz <godsboss@gmx.de>
2024-08-15 20:16:20 +0200

    Add FromSequence.

--- seq/channel/seq.go
+++ seq/channel/seq.go
@@ -1,6 +1,9 @@
 package channel
 
-import "iter"
+import (
+	"iter"
+	"sync"
+)
 
 func ToSequence[Value any](values <-chan Value) iter.Seq[Value] {
 	return func(yield func(value Value) bool) {
@@ -9,5 +12,40 @@ func ToSequence[Value any](values <-chan Value) iter.Seq[Value] {
 				return
 			}
 		}
+	}
+}
+
+// FromSequence creates a channel from a sequence. Values are taken from the sequence and pushed to the channel.
+// After no more values are available, the channel is closed.
+//
+// Call cancel() to free resources and/or to stop taking values from the sequence prematurely. Can be called
+// multiple times, even from different Go routines.
+func FromSequence[Value any](sequence iter.Seq[Value]) (channel <-chan Value, cancel func()) {
+	ch := make(chan Value)
+	closed := make(chan struct{})
+
+	go func() {
+		sequence(
+			func(value Value) bool {
+				select {
+				case ch <- value:
+					return true
+				case <-closed:
+					return false
+				}
+			},
+		)
+
+		close(ch)
+	}()
+
+	once := &sync.Once{}
+
+	return ch, func() {
+		once.Do(
+			func() {
+				close(closed)
+			},
+		)
 	}
 }
--- seq/channel/seq_example_test.go
+++ seq/channel/seq_example_test.go
@@ -2,7 +2,9 @@ package channel_test
 
 import (
 	"fmt"
+	"slices"
 
+	"github.com/GodsBoss/g/seq"
 	"github.com/GodsBoss/g/seq/channel"
 )
 
@@ -21,4 +23,43 @@ func ExampleToSequence() {
 	// This
 	// is
 	// Sparta!
+}
+
+func ExampleFromSequence_exhaustion() {
+	ch, cancel := channel.FromSequence(seq.WithoutKeys(slices.All([]string{"This", "is", "Sparta!"})))
+	defer cancel()
+
+	for line := range ch {
+		fmt.Println(line)
+	}
+
+	// Output:
+	// This
+	// is
+	// Sparta!
+}
+
+func ExampleFromSequence_cancel() {
+	numbers := func(yield func(int) bool) {
+		n := 1
+		for yield(n) {
+			n++
+		}
+	}
+
+	ch, cancel := channel.FromSequence(numbers)
+
+	for n := range ch {
+		if n < 5 {
+			fmt.Printf("Current number is %d.\n", n)
+		} else {
+			cancel()
+		}
+	}
+
+	// Output:
+	// Current number is 1.
+	// Current number is 2.
+	// Current number is 3.
+	// Current number is 4.
 }
