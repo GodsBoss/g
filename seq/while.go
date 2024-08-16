444a0445858e53ba0c2c912d682fe77201b2371a
Timo Reitz <godsboss@gmx.de>
2024-08-16 22:34:40 +0200

    Rename argument.

--- seq/while.go
+++ seq/while.go
@@ -2,12 +2,12 @@ package seq
 
 import "iter"
 
-// While yields values from a sequence as long as the given condition holds true.
-func While[Value any](sequence iter.Seq[Value], condition func(Value) bool) iter.Seq[Value] {
+// While yields values from a sequence as long as the given predicate holds true.
+func While[Value any](sequence iter.Seq[Value], predicate func(Value) bool) iter.Seq[Value] {
 	return func(yield func(Value) bool) {
 		sequence(
 			func(value Value) bool {
-				if condition(value) {
+				if predicate(value) {
 					return yield(value)
 				}
 
