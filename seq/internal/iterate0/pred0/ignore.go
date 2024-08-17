c0a9f9d1013458eff3c21095cddfd24468427479
Timo Reitz <godsboss@gmx.de>
2024-08-17 18:02:04 +0200

    Document Ignore* functions even though they reside in the internal package.

--- seq/internal/iterate0/pred0/ignore.go
+++ seq/internal/iterate0/pred0/ignore.go
@@ -1,11 +1,13 @@
 package pred0
 
+// IgnoreValue takes a nonary predicate and converts it into a unary predicate which ignores its argument.
 func IgnoreValue[Value any](predicate func() bool) func(_ Value) bool {
 	return func(_ Value) bool {
 		return predicate()
 	}
 }
 
+// IgnoreValue takes a nonary predicate and converts it into a binary predicate which ignores its arguments.
 func IgnoreValues[First any, Second any](predicate func() bool) func(_ First, _ Second) bool {
 	return func(_ First, _ Second) bool {
 		return predicate()
