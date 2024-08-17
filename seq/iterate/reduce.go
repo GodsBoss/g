856bf1b9cdde0006684657a2198062a7ec9e9350
Timo Reitz <godsboss@gmx.de>
2024-08-17 14:35:30 +0200

    Make Result the first type parameter.

--- seq/iterate/reduce.go
+++ seq/iterate/reduce.go
@@ -3,7 +3,7 @@ package iterate
 import "iter"
 
 // Reduce reduces values from a sequence into a single value.
-func Reduce[Value any, Result any](initial Result, reducer func(current Result, next Value) Result) func(iter.Seq[Value]) Result {
+func Reduce[Result any, Value any](initial Result, reducer func(current Result, next Value) Result) func(iter.Seq[Value]) Result {
 	return func(sequence iter.Seq[Value]) Result {
 		current := initial
 
