8e99d1a32743910c671d62e800f1999ed9179ef3
Timo Reitz <godsboss@gmx.de>
2024-08-17 14:12:56 +0200

    Add hints to pred2 subpackages.

--- seq/iterate2/while.go
+++ seq/iterate2/while.go
@@ -3,6 +3,8 @@ package iterate2
 import "iter"
 
 // While yields values from a sequence while a predicate holds true.
+//
+// See the pred2 subpackage for pre-defined predicates.
 func While[First any, Second any](predicate func(First, Second) bool) func(iter.Seq2[First, Second]) iter.Seq2[First, Second] {
 	return func(sequence iter.Seq2[First, Second]) iter.Seq2[First, Second] {
 		return func(yield func(first First, second Second) bool) {
