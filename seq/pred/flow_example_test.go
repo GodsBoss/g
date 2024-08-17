c416783f2d718dbe362e263bb296c325eaecd2be
Timo Reitz <godsboss@gmx.de>
2024-08-17 12:10:22 +0200

    Add flow predicates.

---
+++ seq/pred/flow_example_test.go
@@ -0,0 +1,58 @@
+package pred_test
+
+import (
+	"context"
+	"fmt"
+	"slices"
+	"time"
+
+	"github.com/GodsBoss/g/seq"
+	"github.com/GodsBoss/g/seq/pred"
+)
+
+func ExampleContextIsValid() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Cancel the context after 50 milliseconds. In practice a context would more likely be canceled
+	// when a request times out or a server was signalled to be shutdown.
+	go func() {
+		time.Sleep(time.Millisecond * 50)
+		cancel()
+	}()
+
+	items := []int{2, 3, 5, 7, 11, 13, 17, 19}
+
+	for n := range seq.While(slices.Values(items), pred.ContextIsValid[int](ctx)) {
+		fmt.Println(n)
+		time.Sleep(time.Millisecond * 15)
+	}
+
+	// Output:
+	// 2
+	// 3
+	// 5
+	// 7
+}
+
+func ExampleUntilCanceled() {
+	items := []int{2, 3, 5, 7, 11, 13, 17, 19}
+
+	untilCanceled, cancel := pred.UntilCanceled[int]()
+
+	// Cancel after 50 milliseconds.
+	go func() {
+		time.Sleep(time.Millisecond * 50)
+		cancel()
+	}()
+
+	for n := range seq.While(slices.Values(items), untilCanceled) {
+		fmt.Println(n)
+		time.Sleep(time.Millisecond * 15)
+	}
+
+	// Output:
+	// 2
+	// 3
+	// 5
+	// 7
+}
