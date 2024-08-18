8e4ceb77ba24413890a1437dfb7d43660c4d6520
Timo Reitz <godsboss@gmx.de>
2024-08-18 22:02:13 +0200

    Add AND and OR.

---
+++ seq/boolseq/bool.go
@@ -0,0 +1,27 @@
+package boolseq
+
+import "iter"
+
+// And applies a logical AND to all items of a sequence. Returns false if it encounters any false, else true.
+// Stops pulling from sequence as soon as the first false is found.
+func And(sequence iter.Seq[bool]) bool {
+	for b := range sequence {
+		if !b {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Or applies a logical OR to all items of a sequence. Returns true if it encounters any true, else false.
+// Stops pulling from sequence as soon as the first true is found.
+func Or(sequence iter.Seq[bool]) bool {
+	for b := range sequence {
+		if b {
+			return true
+		}
+	}
+
+	return false
+}
---
+++ seq/boolseq/bool_test.go
@@ -0,0 +1,53 @@
+package boolseq_test
+
+import (
+	"iter"
+	"slices"
+	"testing"
+
+	"github.com/GodsBoss/g/seq/boolseq"
+)
+
+func TestBool(t *testing.T) {
+	testcases := map[string]struct {
+		values []bool
+
+		f func(iter.Seq[bool]) bool
+
+		expected bool
+	}{
+		"AND []": {
+			f:        boolseq.And,
+			expected: true,
+		},
+		"AND [true, false]": {
+			values:   []bool{true, false},
+			f:        boolseq.And,
+			expected: false,
+		},
+		"OR []": {
+			f:        boolseq.Or,
+			expected: false,
+		},
+		"OR [false, true]": {
+			values:   []bool{false, true},
+			f:        boolseq.Or,
+			expected: true,
+		},
+	}
+
+	for name := range testcases {
+		testcase := testcases[name]
+
+		t.Run(
+			name,
+			func(t *testing.T) {
+				actual := testcase.f(slices.Values(testcase.values))
+
+				if actual != testcase.expected {
+					t.Errorf("expected %t, got %t", testcase.expected, actual)
+				}
+			},
+		)
+	}
+}
