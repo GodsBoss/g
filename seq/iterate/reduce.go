31c5540fd8554d5bc3ea54e377d94ead03af21c7
Timo Reitz <godsboss@gmx.de>
2024-08-24 20:10:29 +0200

    Warn against passing an infinite iterator to Reduce.

--- seq/iterate/reduce.go
+++ seq/iterate/reduce.go
@@ -3,6 +3,8 @@ package iterate
 import "iter"
 
 // Reduce reduces values from a sequence into a single value.
+//
+// Do not pass infinite iterators as this would lead to Reduce never returning.
 func Reduce[Result any, Value any](initial Result, reducer func(current Result, next Value) Result) func(iter.Seq[Value]) Result {
 	return func(sequence iter.Seq[Value]) Result {
 		current := initial
--- seq/iterate2/reduce.go
+++ seq/iterate2/reduce.go
@@ -3,6 +3,8 @@ package iterate2
 import "iter"
 
 // Reduce reduces values from a sequence into a single value.
+//
+// Do not pass infinite iterators as this would lead to Reduce never returning.
 func Reduce[Result any, First any, Second any](initial Result, reducer func(current Result, first First, second Second) Result) func(iter.Seq2[First, Second]) Result {
 	return func(sequence iter.Seq2[First, Second]) Result {
 		current := initial
