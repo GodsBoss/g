963810efb4d7285d287bcc92b8702b08e2fb66a5
Timo Reitz <godsboss@gmx.de>
2024-08-24 15:46:12 +0200

    Make comment a bit more correct.

--- seq/bitseq/bit.go
+++ seq/bitseq/bit.go
@@ -5,7 +5,7 @@ import (
 	"iter"
 )
 
-// Integer is a constraint for any of the builtin integer types.
+// Integer is a constraint for any of the integer types.
 type Integer interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
