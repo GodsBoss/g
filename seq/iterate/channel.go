59a3795462d229eb56048a5dcd9abbe0473046a0
Timo Reitz <godsboss@gmx.de>
2024-08-24 19:55:13 +0200

    Document IntoChannel's compatability with infinite channels.

--- seq/iterate/channel.go
+++ seq/iterate/channel.go
@@ -26,6 +26,8 @@ func FromChannel[Value any](values <-chan Value) iter.Seq[Value] {
 // IntoChannel creates a channel from a sequence. Values are taken from the sequence and pushed to the channel.
 // After no more values are available, the channel is closed.
 //
+// Safely accepts infinite iterators.
+//
 // Call cancel() to free resources and/or to stop taking values from the sequence prematurely. Can be called
 // multiple times, even from different Go routines.
 func IntoChannel[Value any](sequence iter.Seq[Value]) (channel <-chan Value, cancel func()) {
