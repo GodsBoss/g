5c31e9bf450906c861183053d2f1b7866a3775cc
Timo Reitz <godsboss@gmx.de>
2024-08-24 15:37:54 +0200

    Allow for derived types in Integer and Numeric constraints.

--- seq/bitseq/bit.go
+++ seq/bitseq/bit.go
@@ -7,7 +7,7 @@ import (
 
 // Integer is a constraint for any of the builtin integer types.
 type Integer interface {
-	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64
+	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
 
 // And combines all items from sequence via bitwise AND. Returns -1 (all bits set to 1) if the sequence is empty.
