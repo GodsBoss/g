f6f1b37221b171a80337a585eb7ec53729a2cc2f
Timo Reitz <godsboss@gmx.de>
2024-08-24 19:56:53 +0200

    Mark Empty iterators as both finite and reusable.

--- seq/iterate/empty.go
+++ seq/iterate/empty.go
@@ -1,4 +1,6 @@
 package iterate
 
 // Empty is an iterator which yields no values at all.
+//
+// Obviously finite and reusable.
 func Empty[Value any](_ func(Value) bool) {}
--- seq/iterate2/empty.go
+++ seq/iterate2/empty.go
@@ -1,4 +1,6 @@
 package iterate2
 
 // Empty is an iterator which yields no values at all.
+//
+// Obviously finite and reusable.
 func Empty[First any, Second any](_ func(First, Second) bool) {}
