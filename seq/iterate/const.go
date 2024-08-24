e0b5997fcac811e1f4a346648afb14e4984e1a4b
Timo Reitz <godsboss@gmx.de>
2024-08-24 19:56:06 +0200

    Mark Const iterators as reusable.

--- seq/iterate/const.go
+++ seq/iterate/const.go
@@ -2,7 +2,7 @@ package iterate
 
 // Const creates an iterator that returns the same value repeatedly.
 //
-// Infinite iterator.
+// Infinite iterator. Reusable.
 func Const[First any](first First) func(yield func(First) bool) {
 	return func(yield func(yieldFirst First) bool) {
 		for yield(first) {
--- seq/iterate2/const.go
+++ seq/iterate2/const.go
@@ -2,7 +2,7 @@ package iterate2
 
 // Const creates an iterator that returns the same value pair repeatedly.
 //
-// Infinite iterator.
+// Infinite iterator. Reusable.
 func Const[First any, Second any](first First, second Second) func(yield func(First, Second) bool) {
 	return func(yield func(yieldFirst First, yieldSecond Second) bool) {
 		for yield(first, second) {
