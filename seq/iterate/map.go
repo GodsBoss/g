d0dc3653f41974c690179a5e6377a5bb91fd92f2
Timo Reitz <godsboss@gmx.de>
2024-08-24 20:09:22 +0200

    Document finiteness and reusability for Map iterators.

--- seq/iterate/map.go
+++ seq/iterate/map.go
@@ -3,6 +3,8 @@ package iterate
 import "iter"
 
 // Map maps values from an iter.Seq into other values, given a conversion function.
+//
+// Finiteness and reusability depend on the underlying sequence.
 func Map[Output any, Input any](convert func(Input) Output) func(iter.Seq[Input]) iter.Seq[Output] {
 	return func(sequence iter.Seq[Input]) iter.Seq[Output] {
 		return func(yield func(Output) bool) {
@@ -16,6 +18,8 @@ func Map[Output any, Input any](convert func(Input) Output) func(iter.Seq[Input]
 }
 
 // Map maps values from an iter.Seq2 into other values, given a conversion function.
+//
+// Finiteness and reusability depend on the underlying sequence.
 func Map2[Output any, FirstInput any, SecondInput any](convert func(FirstInput, SecondInput) Output) func(iter.Seq2[FirstInput, SecondInput]) iter.Seq[Output] {
 	return func(sequence iter.Seq2[FirstInput, SecondInput]) iter.Seq[Output] {
 		return func(yield func(Output) bool) {
--- seq/iterate2/map.go
+++ seq/iterate2/map.go
@@ -3,6 +3,8 @@ package iterate2
 import "iter"
 
 // Map maps values from an iter.Seq into other values, given a conversion function.
+//
+// Finiteness and reusability depend on the underlying sequence.
 func Map[FirstOutput any, SecondOutput any, Input any](convert func(Input) (FirstOutput, SecondOutput)) func(iter.Seq[Input]) iter.Seq2[FirstOutput, SecondOutput] {
 	return func(sequence iter.Seq[Input]) iter.Seq2[FirstOutput, SecondOutput] {
 		return func(yield func(FirstOutput, SecondOutput) bool) {
@@ -16,6 +18,8 @@ func Map[FirstOutput any, SecondOutput any, Input any](convert func(Input) (Firs
 }
 
 // Map maps values from an iter.Seq2 into other values, given a conversion function.
+//
+// Finiteness and reusability depend on the underlying sequence.
 func Map2[FirstOutput any, SecondOutput any, FirstInput any, SecondInput any](convert func(FirstInput, SecondInput) (FirstOutput, SecondOutput)) func(iter.Seq2[FirstInput, SecondInput]) iter.Seq2[FirstOutput, SecondOutput] {
 	return func(sequence iter.Seq2[FirstInput, SecondInput]) iter.Seq2[FirstOutput, SecondOutput] {
 		return func(yield func(FirstOutput, SecondOutput) bool) {
