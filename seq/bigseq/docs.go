04f4793fdcdeb717b0e5e4360fd05e7906b4800a
Timo Reitz <godsboss@gmx.de>
2025-07-23 21:07:16 +0200

    Document absence of IntAnd.

--- seq/bigseq/docs.go
+++ seq/bigseq/docs.go
@@ -3,4 +3,8 @@
 //
 // Currently, only big.Int and big.Rat are supported as big.Float handling is a bit more complicated,
 // due to its additional precision and rounding mode.
+//
+// Although Int.Or is supported, Int.And is not as there is no intuitive answer for an empty sequence.
+// For finite integers, all bits are set to 1, but technically big.Int represents arbitrarely large
+// integers, so there is no upper limit to the number of bits.
 package bigseq
