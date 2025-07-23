ed6825d91ed935c4405625ce968eeca80679277d
Timo Reitz <godsboss@gmx.de>
2025-07-23 20:16:29 +0200

    Document behaviour of empty sequences.

--- seq/bigseq/int.go
+++ seq/bigseq/int.go
@@ -5,7 +5,7 @@ import (
 	"math/big"
 )
 
-// IntSum produces the sum of the sequence's integers.
+// IntSum produces the sum of the sequence's integers. If the sequence is empty, zero is returned.
 func IntSum(sequence iter.Seq[*big.Int]) *big.Int {
 	return sum(sequence)
 }
--- seq/bigseq/rat.go
+++ seq/bigseq/rat.go
@@ -5,7 +5,7 @@ import (
 	"math/big"
 )
 
-// RatSum produces the sum of the sequence's rational numbers.
+// RatSum produces the sum of the sequence's rational numbers. If the sequence is empty, zero is returned.
 func RatSum(sequence iter.Seq[*big.Rat]) *big.Rat {
 	return sum(sequence)
 }
