e5f4d8a650e3eafd311a373b13014253edf06295
Timo Reitz <godsboss@gmx.de>
2025-07-23 19:59:20 +0200

    Generify sum of big numbers.

--- seq/bigseq/int.go
+++ seq/bigseq/int.go
@@ -7,11 +7,22 @@ import (
 
 // IntSum produces the sum of the sequence's integers.
 func IntSum(sequence iter.Seq[*big.Int]) *big.Int {
-	result := new(big.Int)
+	return sum[*big.Int](sequence)
+}
+
+func sum[Ptr interface {
+	*T
+	addable[Ptr]
+}, T any](sequence iter.Seq[Ptr]) Ptr {
+	var result Ptr = new(T)
 
 	for n := range sequence {
 		result = result.Add(result, n)
 	}
 
 	return result
+}
+
+type addable[T any] interface {
+	Add(T, T) T
 }
