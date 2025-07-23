047761c82228e118de6e3ef079e8f8356a510da8
Timo Reitz <godsboss@gmx.de>
2025-07-23 21:08:54 +0200

    Enhance package intro by removing references to the types, especially since big.Float isn't supported yet.

--- seq/bigseq/docs.go
+++ seq/bigseq/docs.go
@@ -1,5 +1,4 @@
-// Package bigseq handles types of the math/big package, namely big.Int, big.Rat and big.Float,
-// in an iterator way.
+// Package bigseq handles types of the math/big package in a Go 1.23 iterator way.
 //
 // Currently, only big.Int and big.Rat are supported as big.Float handling is a bit more complicated,
 // due to its additional precision and rounding mode.
